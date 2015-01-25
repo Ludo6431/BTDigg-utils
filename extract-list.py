ef1e6cc1f6bfcc9e3ed677c00cccdcea7f54714e
Ludovic Lacoste <ludolacost@gmail.com>
2015-01-25 22:06:59 +0100

    Extracts hash from url

--- extract-list.py
+++ extract-list.py
@@ -5,6 +5,8 @@
 from enum import Enum
 from html.parser import HTMLParser
 from urllib.request import urlopen
+from re import compile
+from cgi import escape
 
 class ParseState(Enum):
     Unknown = 0
@@ -28,25 +30,33 @@ class ParseState(Enum):
         return 0
 
 class BtDiggTop100Entry:
-    rank = -1
-    dlcount = -1
-    size = -1
-    filecount = -1
-    fakecount = -1
-    name = ""
-    url = ""
+    url = "http://btdigg.org"
+    hash_extractor = compile(".*info_hash=([0-9a-fA-F]+)")
     
     def __str__(self):
-        return "[rk=" + str(self.rank) + " ; dlcnt=" + str(self.dlcount) + " ; size=" + str(self.size) + " ; filecnt=" + str(self.filecount) + " ; fakecnt=" + str(self.fakecount) + " ; name=" + str(self.name) + " ; url=" + str(self.url) + "]"
+        return "[rk=" +     str(self.rank) +\
+            " ; dlcnt=" +   str(self.dlcount) +\
+            " ; size=" +    str(self.size) +\
+            " ; filecnt=" + str(self.filecount) +\
+            " ; fakecnt=" + str(self.fakecount) +\
+            " ; name=" +    str(self.name) +\
+            " ; url=" +     str(self.url) + "]"
     
+    def to_magnet(self):
+        hash = get_hash()
+        return "magnet:?xt=urn:btih:" + hash # + "&dn=" + escape(self.name) FIXME add escaped name
+
+    def get_hash(self):
+        p = BtDiggTop100Entry.hash_extractor.search(self.url)
+        return p.group(1)
+
 class BtDiggTop100Parser(HTMLParser):
     state = ParseState.Unknown
     verbose = 0
     curr_entry = None
-    url_prefix = "http://btdigg.org"
 
-    def handle_row(self, r):
-        pass
+    def handle_row(self, e):
+        print("magnet:", e.to_magnet())
 
     def handle_starttag(self, tag, attrs):
         if self.verbose > 2:
@@ -67,9 +77,11 @@ class BtDiggTop100Parser(HTMLParser):
         elif ParseState.isParsingRow(self.state):
             if self.state == ParseState.Row_Name and tag == "a":
                 d = dict(attrs)
-                url = self.url_prefix + d["href"]
+                url = BtDiggTop100Entry.url + d["href"]
+                
                 if self.verbose > 1:
                     print("url      :", url)
+                    
                 self.curr_entry.url = url
         else:
             self.state = ParseState.Unknown
@@ -90,21 +102,25 @@ class BtDiggTop100Parser(HTMLParser):
         elif self.state == ParseState.Row:
             if self.verbose > 1:
                 print("rank     :", data)
+                
             self.curr_entry.rank = int(data)
             self.state = ParseState.Row_DLCount
         elif self.state == ParseState.Row_DLCount:
             if self.verbose > 1:
                 print("dlcount  :", data)
+                
             self.curr_entry.dlcount = int(data)
             self.state = ParseState.Row_Size1
         elif self.state == ParseState.Row_Size1:
             if self.verbose > 1:
                 print("size1    :", data)
+                
             self.curr_entry.size = float(data)
             self.state = ParseState.Row_Size2
         elif self.state == ParseState.Row_Size2:
             if self.verbose > 1:
                 print("size2    :", data)
+                
             if data == "KB":
                 self.curr_entry.size = self.curr_entry.size * 1000
             elif data == "MB":
@@ -117,18 +133,22 @@ class BtDiggTop100Parser(HTMLParser):
         elif self.state == ParseState.Row_FileCount:
             if self.verbose > 1:
                 print("filecount:", data)
+                
             self.curr_entry.filecount = int(data)
             self.state = ParseState.Row_FakeCount
         elif self.state == ParseState.Row_FakeCount:
             if self.verbose > 1:
                 print("fakecount:", data)
+                
             self.curr_entry.fakecount = data
             self.state = ParseState.Row_Name
         elif self.state == ParseState.Row_Name:
             if self.verbose > 1:
                 print("name     :", data)
+                
             self.curr_entry.name = data
             print("entry:", self.curr_entry)
+            self.handle_row(self.curr_entry)
             self.curr_entry = BtDiggTop100Entry()
             self.state = ParseState.Row
 
