70316df821ef67aa7a211c59740f35e2cc2126f8
Ludovic Lacoste <ludolacost@gmail.com>
2015-01-27 00:28:27 +0100

    Adds CLI sqlite3

---
+++ src/pysql.py
@@ -0,0 +1,35 @@
+'''
+Created on 26 janv. 2015
+
+@author: ludo6431
+'''
+# A minimal SQLite shell for experiments
+
+import sqlite3
+
+con = sqlite3.connect("top100.db")
+con.isolation_level = None
+cur = con.cursor()
+
+buffer = ""
+
+print("Enter your SQL commands to execute in sqlite3.")
+print("Enter a blank line to exit.")
+
+while True:
+    line = input()
+    if line == "":
+        break
+    buffer += line
+    if sqlite3.complete_statement(buffer):
+        try:
+            buffer = buffer.strip()
+            cur.execute(buffer)
+
+            if buffer.lstrip().upper().startswith("SELECT"):
+                print(cur.fetchall())
+        except sqlite3.Error as e:
+            print("An error occurred:", e.args[0])
+        buffer = ""
+
+con.close()
