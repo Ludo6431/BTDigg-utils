44cdf9656d769eafd792014db28d55d5f55a082e
Ludovic Lacoste <ludolacost@gmail.com>
2015-02-05 22:12:01 +0100

    Uses two cursors for parallel queries

--- src/db_show.py
+++ src/db_show.py
@@ -9,10 +9,12 @@ import sqlite3
 if __name__ == '__main__':
     conn = sqlite3.connect("top100.db", detect_types=sqlite3.PARSE_DECLTYPES)
     
-    c = conn.cursor()
-    for t in list(c.execute('SELECT * FROM torrent')):
+    c1 = conn.cursor()
+    c2 = conn.cursor()
+    
+    for t in c1.execute('SELECT * FROM torrent'):
         print("torrent:", t[1])
-        for r in c.execute('SELECT rank, dlcount FROM top_entry WHERE torrent_hash = ?', (t[0],)):
+        for r in c2.execute('SELECT rank, dlcount FROM top_entry WHERE torrent_hash = ?', (t[0],)):
             print("  rank:", r[0], 'dlcount:', r[1])
 
     conn.close()
