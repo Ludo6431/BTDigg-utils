eaec88a6754bb96d45267db1eef51cb0f2b72049
Ludovic Lacoste <ludolacost@gmail.com>
2015-02-05 22:13:48 +0100

    Adds simple messy data plotting with matplotlib

--- src/db_show.py
+++ src/db_show.py
@@ -6,15 +6,26 @@ Created on 27 janv. 2015
 
 import sqlite3
 
+import matplotlib.pyplot as plt
+
+
 if __name__ == '__main__':
     conn = sqlite3.connect("top100.db", detect_types=sqlite3.PARSE_DECLTYPES)
-    
+     
     c1 = conn.cursor()
     c2 = conn.cursor()
-    
+    plt.hold(True)
+
     for t in c1.execute('SELECT * FROM torrent'):
         print("torrent:", t[1])
-        for r in c2.execute('SELECT rank, dlcount FROM top_entry WHERE torrent_hash = ?', (t[0],)):
+        l = []
+        for r in c2.execute('SELECT rank, dlcount, top_date FROM top_entry WHERE torrent_hash = ?', (t[0],)):
             print("  rank:", r[0], 'dlcount:', r[1])
-
+            l.append(r)
+        x = [r[2] for r in l]
+        y = [r[1] for r in l]
+        plt.plot(x, y)
+ 
     conn.close()
+
+    plt.show()
