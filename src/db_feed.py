ba122e5f79030afed5f0ecfc997bd4ecaa3a67d4
Ludovic Lacoste <ludolacost@gmail.com>
2015-02-16 20:55:58 +0100

    Updates to spoof User-agent

--- src/db_feed.py
+++ src/db_feed.py
@@ -4,7 +4,7 @@
 
 import sqlite3
 from sqlite3.dbapi2 import Timestamp
-from urllib.request import urlopen
+from urllib.request import urlopen,Request
 
 from btdigg.top100parser import BtDiggTop100Parser
 
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     conn = sqlite3.connect("top100.db", detect_types=sqlite3.PARSE_DECLTYPES)
     d = Timestamp.utcnow()
     
-    html = str(urlopen("http://btdigg.org/top100.html").read())
+    html = str(urlopen(Request("http://btdigg.org/top100.html", headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.2; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0"})).read())
 
     p = ParserAddToDB(d, conn.cursor())
     p.feed(html)
